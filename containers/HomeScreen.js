5746975b9571f55eca857a3dec16b3a4a5a42b01
Anat Plt <108489401+Anat76@users.noreply.github.com>
2023-05-17 20:04:07 +0200

    Update HomeScreen.js

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -1,17 +1,38 @@
 import { useNavigation } from "@react-navigation/core";
 import { Button, Text, View } from "react-native";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
+
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-bootcamp-api.herokuapp.com/api/airbnb/rooms"
+        );
+        console.log("importé");
+        setData(response.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <View>
-      <Text>Welcome home!</Text>
-      <Button
-        title="Go to Profile"
-        onPress={() => {
-          navigation.navigate("Profile", { userId: 123 });
-        }}
-      />
-    </View>
+    <View></View>
+    // <View>
+    //   <Text>Welcome home!</Text>
+    //   <Button
+    //     title="Go to Profile"
+    //     onPress={() => {
+    //       navigation.navigate("Profile", { userId: 123 });
+    //     }}
+    //   />
+    // </View>
   );
 }
