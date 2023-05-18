8ef79bd1eedbf66f2d2db01483c19cebb6691f80
Anat Plt <108489401+Anat76@users.noreply.github.com>
2023-05-18 22:32:58 +0200

    Update HomeScreen.js

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -1,5 +1,14 @@
 import { useNavigation } from "@react-navigation/core";
-import { Button, Text, View } from "react-native";
+import {
+  Button,
+  Text,
+  View,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { ActivityIndicator } from "react-native-paper";
@@ -8,7 +17,7 @@ export default function HomeScreen() {
   const navigation = useNavigation();
 
   const [data, setData] = useState("");
-  const [isLoading, setIsLoadding] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,9 +25,9 @@ export default function HomeScreen() {
         const response = await axios.get(
           "https://lereacteur-bootcamp-api.herokuapp.com/api/airbnb/rooms"
         );
-        console.log("importé");
+        // console.log("importé");
         setData(response.data);
-        setIsLoadding(false);
+        setIsLoading(false);
       } catch (error) {
         console.log(error.message);
       }
@@ -27,16 +36,69 @@ export default function HomeScreen() {
   }, []);
 
   return isLoading ? (
-    <ActivityIndicator />
+    <ActivityIndicator size="large" />
   ) : (
-    <View>
-      <Text>Welcome home!</Text>
-      <Button
-        title="Go to Profile"
-        onPress={() => {
-          navigation.navigate("Profile", { userId: 123 });
+    <View style={StyleSheet.container}>
+      <FlatList
+        data={data}
+        keyExtractor={(data) => data._id}
+        renderItem={(data) => {
+          // console.log(data.item);
+          // console.log(data.item.photos[0].url);
+          // console.log(data.item.user.account.photo.url);
+          return (
+            <TouchableOpacity
+              onPress={() => {
+                console.log("Room offer");
+              }}
+            >
+              <ImageBackground
+                source={{ uri: data.item.photos[0].url }}
+                style={styles.ImgRoom}
+              >
+                <Text style={styles.price}>{data.item.price} €</Text>
+              </ImageBackground>
+              <View>
+                <Text>{data.item.title}</Text>
+                <View>
+                  <Text>{data.item.reviews}</Text>
+                </View>
+                <Image
+                  source={{ uri: data.item.user.account.photo.url }}
+                  style={styles.avatar}
+                />
+              </View>
+            </TouchableOpacity>
+          );
         }}
+        ItemSeparatorComponent={() => <Text style={styles.separator}></Text>}
       />
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: { padding: 10 },
+  ImgRoom: {
+    width: "100%",
+    height: 150,
+    justifyContent: "flex-end",
+  },
+  price: {
+    color: "white",
+    padding: 10,
+    backgroundColor: "#1E1E1E",
+    width: 70,
+    marginBottom: 10,
+    fontWeight: "bold",
+  },
+  separator: {
+    backgroundColor: "grey",
+    height: 1,
+    marginVertical: 10,
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+});
